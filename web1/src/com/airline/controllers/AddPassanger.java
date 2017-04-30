7625a7baa0360af589915067fdea110b0325c0c4
Milos <mpmilos@yahoo.com>
2017-04-30 15:28:34 +0200

    Created a servlet for adding passengers to a flight

---
+++ web1/src/com/airline/controllers/AddPassanger.java
@@ -0,0 +1,49 @@
+package com.airline.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AddPassanger
+ */
+@WebServlet("/AddPassanger")
+public class AddPassanger extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddPassanger() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		out.println("<html><body>");
+		out.println("<h2>Welcome to Adventures Airlines</h2>");
+		out.append("Served at: ").append(request.getContextPath());
+		out.println("</body></html>");
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
